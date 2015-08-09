b76084145ab932cf9fd7dd51671e5c91972d0c57
Sahand Hariri <sahandha@gmail.com>
2015-08-09 15:41:57 -0500

    first commit

---
+++ SIR/Economy.cpp
@@ -0,0 +1,9 @@
+//
+//  Economy.cpp
+//  SIR
+//
+//  Created by Sahand Hariri on 8/9/15.
+//  Copyright (c) 2015 Sahand Hariri. All rights reserved.
+//
+
+#include "Economy.h"
---
+++ SIR/Economy.h
@@ -0,0 +1,33 @@
+//
+//  Economy.h
+//  SIR
+//
+//  Created by Sahand Hariri on 8/9/15.
+//  Copyright (c) 2015 Sahand Hariri. All rights reserved.
+//
+
+#ifndef __SIR__Economy__
+#define __SIR__Economy__
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <list>
+#include "Place.h"
+#include "Disease.h"
+#include "InHostDynamics.h"
+
+using namespace std;
+
+class Economy {
+public:
+    
+    Economy();
+    
+    
+private:
+    
+};
+
+
+#endif /* defined(__SIR__Economy__) */
